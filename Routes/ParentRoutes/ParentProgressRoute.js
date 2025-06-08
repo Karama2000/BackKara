f56aea91a005e70190a81cc20611768e56e66220
Karama Mighri <karamamighry@gmail.com>
2025-06-08 21:14:51 +0100

    Fix77

--- Routes/ParentRoutes/ParentProgressRoute.js
+++ Routes/ParentRoutes/ParentProgressRoute.js
@@ -4,13 +4,12 @@ const parentProgressController = require('../../Controllers/ParentControllers/Pa
 const parentController = require('../../Controllers/ParentControllers/ParentController');
 const authMiddleware = require('../../MiddleWare/protectRoute');
 
-router.get('/parent/children', authMiddleware, parentController.getChildren);
-router.get('/parent/progress', authMiddleware, parentController.getChildrenProgress);
-router.delete('/parent/progress', authMiddleware, parentController.deleteChildrenProgress);
-router.get('/parent/notifications', authMiddleware, parentController.getParentNotifications);
-router.post('/parent/notifications/:id/read', authMiddleware, parentController.markNotificationAsRead);
-router.delete('/parent/notifications/:id', authMiddleware, parentController.deleteParentNotification);
-router.delete('/parent/notifications', authMiddleware, parentController.deleteAllParentNotifications);
+router.get('/parent/children', authMiddleware, parentProgressController.getChildren);
+router.get('/parent/progress', authMiddleware, parentProgressController.getChildrenProgress);
+router.delete('/parent/progress', authMiddleware, parentProgressController.deleteChildrenProgress);
+router.get('/parent/notifications', authMiddleware, parentProgressController.getParentNotifications);
+router.post('/parent/notifications/:id/read', authMiddleware, parentProgressController.markNotificationAsRead);
+router.delete('/parent/notifications/:id', authMiddleware, parentProgressController.deleteParentNotification);
+router.delete('/parent/notifications', authMiddleware, parentProgressController.deleteAllParentNotifications);
 
-module.exports = router;
 module.exports = router;
